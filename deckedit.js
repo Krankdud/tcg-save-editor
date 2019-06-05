34eea93b33a54a70ef46e6b590c497536538f38b
charlie <charlieschuy@gmail.com>
2019-06-04 19:08:29 -0700

    Add custom decks

--- deckedit.js
+++ deckedit.js
@@ -1,19 +1,119 @@
-$(document).ready(function() {
+var deck = {}
+
+$(document).ready(function () {
     var cardsSource = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
-        identity: function(obj) { return obj.name },
+        identity: function (obj) { return obj.name },
         local: cards
     });
 
-    $('#card').typeahead({
+    var deckTotal = 0;
+    var cardTemplate = _.template(`
+        <div id="<%= cardId %>" class="row tcg-card">
+            <div class="col-8">
+                <button id="<%= cardId %>-close" type="button" class="close tcg-close mr-3" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <%= name %>
+            </div>
+            <div class="col">
+                <button id="<%= cardId %>-decrement" class="btn btn-secondary btn-sm">-</button>
+                <span id="<%= cardId %>-count" class="ml-2 mr-2">1</span>
+                <button id="<%= cardId %>-increment" class="btn btn-secondary btn-sm">+</button>
+            </div>
+        </div>
+    `);
+    var nextCardId = 0;
+
+    $('#tcg-card-input').typeahead({
         hint: true,
         highlight: true,
         minLength: 1
     },
-    {
-        name: 'cards',
-        display: 'name',
-        source: cardsSource
-    });
+        {
+            name: 'cards',
+            display: 'name',
+            source: cardsSource
+        });
+    $('#tcg-add-card').click(function () {
+        addCard($('#tcg-card-input').typeahead('val'));
+    })
+    $('#tcg-card-input').keypress(function (e) {
+        if (e.which === 13) {
+            $(this).attr("disabled", "disabled")
+
+            addCard($('#tcg-card-input').typeahead('val'));
+
+            $(this).removeAttr("disabled");
+        }
+    })
+
+    function addCard(name) {
+        if (deck[name] !== undefined || deckTotal >= 60) {
+            return;
+        }
+
+        var card = _.findWhere(cards, { name: name });
+        if (card == undefined) {
+            return
+        }
+
+        deck[name] = {
+            name: name,
+            byte: card.byte,
+            count: 1
+        };
+        deckTotal++;
+
+        createCardElement(name);
+        $('#deck-count').text(deckTotal);
+    }
+
+    function createCardElement(name) {
+        var card = deck[name];
+        if (card === undefined) {
+            return;
+        }
+
+        var cardId = "tcg-card-" + getNextCardId();
+        var cardHtml = cardTemplate({
+            name: card.name,
+            cardId: cardId
+        });
+
+        $('#deck-edit-cards').append(cardHtml);
+
+        var cardSelector = "#" + cardId;
+        $(cardSelector + '-close').click(function () {
+            deckTotal -= deck[card.name].count;
+            deck[card.name] = undefined;
+            $(cardSelector).detach();
+            $('#deck-count').text(deckTotal);
+        });
+        $(cardSelector + '-decrement').click(function () {
+            deck[card.name].count--;
+            deckTotal--;
+            if (deck[card.name].count <= 0) {
+                deck[card.name] = undefined;
+                $(cardSelector).detach();
+            } else {
+                $(cardSelector + '-count').text(deck[card.name].count);
+                $('#deck-count').text(deckTotal);
+            }
+        });
+        $(cardSelector + '-increment').click(function () {
+            if (deckTotal >= 60) {
+                return;
+            }
+            deck[card.name].count++;
+            deckTotal++;
+            $(cardSelector + '-count').text(deck[card.name].count);
+            $('#deck-count').text(deckTotal);
+        });
+    }
+
+    function getNextCardId() {
+        return nextCardId++;
+    }
 });
--- save_editor.js
+++ save_editor.js
@@ -1,77 +1,13 @@
 import generateBaseSave from './base_state.js';
 
-function convertToHex(s) {
-    let conversionTable = {
-        "A": [0x03, 0x30],
-        "B": [0x03, 0x31],
-        "C": [0x03, 0x32],
-        "D": [0x03, 0x33],
-        "E": [0x03, 0x34],
-        "F": [0x03, 0x35],
-        "G": [0x03, 0x36],
-        "H": [0x03, 0x37],
-        "I": [0x03, 0x38],
-        "J": [0x03, 0x39],
-        "K": [0x03, 0x3A],
-        "L": [0x03, 0x3B],
-        "M": [0x03, 0x3C],
-        "N": [0x03, 0x3D],
-        "O": [0x03, 0x3E],
-        "P": [0x03, 0x3F],
-        "Q": [0x03, 0x40],
-        "R": [0x03, 0x41],
-        "S": [0x03, 0x42],
-        "T": [0x03, 0x43],
-        "U": [0x03, 0x44],
-        "V": [0x03, 0x45],
-        "W": [0x03, 0x46],
-        "X": [0x03, 0x47],
-        "Y": [0x03, 0x48],
-        "Z": [0x03, 0x49],
-        "!": [0x0E, 0x6E],
-        "?": [0x0E, 0x6F],
-        "&": [0x03, 0x5D],
-        "+": [0x0E, 0x6A],
-        "-": [0x0E, 0x6B],
-        ".": [0x0E, 0x77],
-        "0": [0x0E, 0x60],
-        "1": [0x0E, 0x61],
-        "2": [0x0E, 0x62],
-        "3": [0x0E, 0x63],
-        "4": [0x0E, 0x64],
-        "5": [0x0E, 0x65],
-        "6": [0x0E, 0x66],
-        "7": [0x0E, 0x67],
-        "8": [0x0E, 0x68],
-        "9": [0x0E, 0x69],
-    };
-    var name = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    // Hard limit to 6 since names are 6 characters long
-    var len = s.length > 6 ? 6 : s.length;
-    for (var i = 0; i < len; i++) {
-        var hex = conversionTable[s[i]];
-        name[i * 2] = hex[0];
-        name[i * 2 + 1] = hex[1];
-    }
-    return name;
-}
-
-function createSave(name) {
-    var base = generateBaseSave();
-    for (var i = 0; i < name.length; i++) {
-        base[0xC627 + i] = name[i];
-    }
-    return base;
-}
-
-window.addEventListener("load", function() {
+$(document).ready(function () {
     var nameInput = document.getElementById("name");
     var createButton = document.getElementById("create");
 
-    createButton.onclick = function() {
+    createButton.onclick = function () {
         var save = createSave(convertToHex(nameInput.value.toUpperCase()));
         var bytes = new Uint8Array(save);
-        var blob = new Blob([bytes], {type: "application/octet-stream"});
+        var blob = new Blob([bytes], { type: "application/octet-stream" });
         var link = document.createElement("a");
         link.download = "Pokemon TCG Save.sn1";
         link.href = window.URL.createObjectURL(blob);
@@ -79,4 +15,79 @@ window.addEventListener("load", function() {
         link.click();
         document.body.removeChild(link);
     };
+
+    function createSave(name) {
+        var base = generateBaseSave();
+        for (var i = 0; i < name.length; i++) {
+            base[0xC627 + i] = name[i];
+        }
+
+        if ($("#enable-deck-edit").prop("checked")) {
+            var address = 0x1082F;
+            _.each(deck, function (card, key) {
+                for (var i = 0; i < card.count; i++) {
+                    base[address] = card.byte;
+                    address++;
+                }
+            });
+        }
+
+        return base;
+    }
+
+    function convertToHex(s) {
+        let conversionTable = {
+            "A": [0x03, 0x30],
+            "B": [0x03, 0x31],
+            "C": [0x03, 0x32],
+            "D": [0x03, 0x33],
+            "E": [0x03, 0x34],
+            "F": [0x03, 0x35],
+            "G": [0x03, 0x36],
+            "H": [0x03, 0x37],
+            "I": [0x03, 0x38],
+            "J": [0x03, 0x39],
+            "K": [0x03, 0x3A],
+            "L": [0x03, 0x3B],
+            "M": [0x03, 0x3C],
+            "N": [0x03, 0x3D],
+            "O": [0x03, 0x3E],
+            "P": [0x03, 0x3F],
+            "Q": [0x03, 0x40],
+            "R": [0x03, 0x41],
+            "S": [0x03, 0x42],
+            "T": [0x03, 0x43],
+            "U": [0x03, 0x44],
+            "V": [0x03, 0x45],
+            "W": [0x03, 0x46],
+            "X": [0x03, 0x47],
+            "Y": [0x03, 0x48],
+            "Z": [0x03, 0x49],
+            "!": [0x0E, 0x6E],
+            "?": [0x0E, 0x6F],
+            "&": [0x03, 0x5D],
+            "+": [0x0E, 0x6A],
+            "-": [0x0E, 0x6B],
+            ".": [0x0E, 0x77],
+            "0": [0x0E, 0x60],
+            "1": [0x0E, 0x61],
+            "2": [0x0E, 0x62],
+            "3": [0x0E, 0x63],
+            "4": [0x0E, 0x64],
+            "5": [0x0E, 0x65],
+            "6": [0x0E, 0x66],
+            "7": [0x0E, 0x67],
+            "8": [0x0E, 0x68],
+            "9": [0x0E, 0x69],
+        };
+        var name = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        // Hard limit to 6 since names are 6 characters long
+        var len = s.length > 6 ? 6 : s.length;
+        for (var i = 0; i < len; i++) {
+            var hex = conversionTable[s[i]];
+            name[i * 2] = hex[0];
+            name[i * 2 + 1] = hex[1];
+        }
+        return name;
+    }
 });
